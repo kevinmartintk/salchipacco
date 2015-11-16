241f478c30b2c296d9d1a4d092ecf2b0846d1f3b
Inobtenio <kevin.martin@tektonlabs.com>
2015-11-16 18:27:13 -0500

    asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_controller.asset_host = ENV['HOST'] + ENV['RELATIVE_URL_ROOT']
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
